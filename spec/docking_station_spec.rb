70387a20d7b776384cca8945e99be2632ab4438d
samcolson4 <samcolson4@gmail.com>
2020-09-02 17:33:36 +0100

    Update tests to see if new Bike is created from Docking Station when asked to release bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,9 @@ describe DockingStation do
     docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
   end
+
+  it "gets the bike, and checks if it is working" do
+    station = DockingStation.new
+    expect(station.release_bike).to eq bike = Bike.new
+  end
 end
