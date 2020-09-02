ef7f70bc3e772f743b0037a67a8f374bbf0ff5e1
Student <student@Makerss-Air.broadband>
2020-09-02 18:01:33 +0100

    Add test to docking station to see if bike release and working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,6 +8,8 @@ describe DockingStation do
 
   it "gets the bike, and checks if it is working" do
     station = DockingStation.new
-    expect(station.release_bike).to eq bike = Bike.new
+    expect(station.release_bike).to be_kind_of(Bike)
+    bike = Bike.new
+    expect(bike.working?).to eq true
   end
 end
